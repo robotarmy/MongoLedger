9277b64af10473814467a0814b066fa2378e1d6a
Curtis J Schofield <github.com@robotarmyma.de>
2010-06-15 18:07:01 -0700

    yay
    run 10 times

--- public/test/multi_benchmark_qu.js
+++ public/test/multi_benchmark_qu.js
@@ -5,10 +5,20 @@
     urls = [
       'http://ledger.robotarmyma.de:7001',
       'http://ledger.robotarmyma.de:5000/ledgers',
-       'http://ledger.robotarmyma.de:5001/ledgers',
-        'http://ledger.robotarmyma.de:5002/ledgers',
-           'http://ledger.robotarmyma.de:5003/ledgers'
+      'http://ledger.robotarmyma.de:5001/ledgers',
+      'http://ledger.robotarmyma.de:5002/ledgers',
+      'http://ledger.robotarmyma.de:5003/ledgers'
     ];
+    LABELS = [
+      'NODE.JS + node-mongo-native driver + glue',
+      '1 Thin Rails + Mongomapper ',
+      '1 Thin Rails + Mysql',
+      '1 Thin Rails + PostGreSQL',
+      '1 Thin Rails + SQLITE'
+    ];
+    
+    var NumberOfCalls = 100;
+
     var doc = {
       name: 'This is a String value',
       name2: 'This is a String value2',
@@ -16,9 +26,34 @@
       number2: 199042,
       counter: 0
     };
-    var j = 0;
+    var fullGraph = function(records) {
+      if (this._history == undefined){
+        this._history = [];
+        this._x = [];
+      }
+      var hlength = this._history.length;
+      this._history[hlength] = [];
+      for (el in records) {
+        this._history[hlength][this._history[hlength].length] = records[el].delta;
+      }
+      var x = [];
+      for(var i = 0; i < NumberOfCalls; i++){
+        x[i] = i+1 ;
+      }
+      this._x[this._x.length] = x;
+      if (this._history.length == urls.length)
+        {
+          var graph_id = new Date().getTime();
+          var div = $("<div class='all_graph' id='graph_"+graph_id+"'></div>");
+          $('#all_graph').append(div);
+          var name = "All Servers rountrip Time in MS ";
+          new Graph().graph_line(name,'graph_'+graph_id,this._x,this._history,LABELS);  
+          this._history = undefined; 
+        }  
+    };
+    for(j = 0; j < 10; j++) {
     for(i = 0; i < urls.length; i++) {
-      (function(url) { 
+      (function(url,fg) { 
         module("["+url+"] Insert");
         var insert = new Benchmark();
         var callback = function(current_count) {
@@ -36,21 +71,22 @@
             }
           });
         }
-        asyncTest("POST 10", 0, function(){
+        asyncTest("POST ", 0, function(){
           var at = this;
           var finished_callback = function() {
             var self = this;
-            var graph_id = new Date().getTime();
-            var div = $("<div id='"+graph_id+"'><div id='graph_"+graph_id+"'></div></div>");
-            $('body article').append(div);
-            self.results('body article #'+graph_id);
-            new Graph().graph('graph_'+graph_id,self.records,500);  
+          //  var graph_id = new Date().getTime();
+          //  var div = $("<div id='"+graph_id+"'><div class='graph' id='graph_"+graph_id+"'></div></div>");
+          //  $('body article').append(div);
+          //  self.results('body article #'+graph_id);
+          //  new Graph().graph('graph_'+graph_id,self.records,500);  
+            fg(self.records);
             start();
           };
-          insert.run(100,url,callback,finished_callback);
-
+          insert.run(NumberOfCalls,url,callback,finished_callback);
         });
-      })(urls[i]);
+      })(urls[i],fullGraph);
+    }
     }
   });
 })();
